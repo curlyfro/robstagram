c78768ba4b922940ee8a91c37867bd784ea7b3ae
rschw <38385263+rschw@users.noreply.github.com>
2018-10-08 21:46:48 +0200

    added api base url injection for debug mode

--- ClientApp/src/main.ts
+++ ClientApp/src/main.ts
@@ -3,13 +3,16 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { API_BASE_URL } from './app/api/api.service.generated';
 
 export function getBaseUrl() {
-  return document.getElementsByTagName('base')[0].href;
+  // return document.getElementsByTagName('base')[0].href;
+  return environment.API_BASE_URL;
 }
 
 const providers = [
-  { provide: 'BASE_URL', useFactory: getBaseUrl, deps: [] }
+  { provide: 'BASE_URL', useFactory: getBaseUrl, deps: [] },
+  { provide: API_BASE_URL, useFactory: getBaseUrl, deps: [] }
 ];
 
 if (environment.production) {
